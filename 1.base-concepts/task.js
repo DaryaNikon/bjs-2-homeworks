13ca24dca9192468502c701ce4497cd8f0014728
Darya Nikonova <luck.my9@yandex.ru>
2025-01-23 02:46:57 +0300

    всё?

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,32 +2,27 @@
 
 function solveEquation(variableA, variableB, variableC) {
 	let arr = [];
-	let root1;
-	let root2;
 	let discriminant = variableB ** 2 - 4 * variableA * variableC;
 
 	if (discriminant > 0) {
-		root1 = (-variableB + Math.sqrt(discriminant)) / (2 * variableA);
-		root2 = (-variableB - Math.sqrt(discriminant)) / (2 * variableA);
+		let root1 = (-variableB + Math.sqrt(discriminant)) / (2 * variableA);
+		let root2 = (-variableB - Math.sqrt(discriminant)) / (2 * variableA);
 		arr.push(root1, root2);
 	} else if (discriminant === 0) {
-		root1 = -variableB / (2 * variableA);
+		let root1 = -variableB / (2 * variableA);
 		arr.push(root1);
 	}
 	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	let S = amount - contribution;
-	let P = percent / 100 / 12;
+	let loanBody = amount - contribution;
+	let percentagePerMonth = percent / 100 / 12;
 	let payment;
 	let credit;
 
-	if (amount > 0) {
-		payment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
-		credit = (payment * countMonths);
-
-		alert(+credit.toFixed(2));
-	}
+	payment = loanBody * (percentagePerMonth + (percentagePerMonth / (((1 + percentagePerMonth) ** countMonths) - 1)));
+	credit = (payment * countMonths);
+	
 	return +credit.toFixed(2);
 }
