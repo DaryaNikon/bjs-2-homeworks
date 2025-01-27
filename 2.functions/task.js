d746ab93139110340eb67bfe6fa46a6f23f3b258
Darya Nikonova <luck.my9@yandex.ru>
2025-01-27 03:06:39 +0300

    Второе задание

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,6 +1,23 @@
 function getArrayParams(...arr) {
+  let min = -Infinity;
+  let max = Infinity;
+  let sum = 0;
+  let avg;
 
-  return { min: min, max: max, avg: avg };
+  for (let i = 0; i < max; i++) {
+    max = i;
+  };  
+
+  for (let i = 0; i > min; i++) {
+    min = i;
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    avg = sum / arr.length;
+  };
+
+  return { min: min, max: max, avg: +avg.toFixed(2) };
 }
 
 function summElementsWorker(...arr) {
